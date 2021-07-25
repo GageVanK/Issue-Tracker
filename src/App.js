600aeb04c36e7fb207566b07d19902b3fe786544
gagev <gagev@LAPTOP-I1RO59UF>
2021-07-25 00:58:01 -0400

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import AddIssue from './Components/AddIssue/AddIssue.js';
+import CurrentIssue from './Components/CurrentIssue/CurrentIssue.js';
+
+import './App.css';
+
+function App() {
+    const [addIssue, setAddIssue] = useState(true);
+
+    function showAddIssue() {
+        setAddIssue(true);
+    }
+
+    function showCurrentIssue() {
+        setAddIssue(false);
+    }
+    
+  return (
+      <div className="App">
+          <h1>Issue Tracker</h1>
+          <div className="grey-card-container">
+              {addIssue ? <AddIssue /> : <CurrentIssue />}
+          </div>
+          <div className="view-selection-div">
+              <button class="view-button" onClick={() => showCurrentIssue()}>
+                  Current Issues
+              </button>
+
+              <button class="view-button active-button" onClick={() => showAddIssue()}>
+                  Add Issue
+              </button>
+          </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/AddIssue/AddIssue.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import "./AddIssue.css";
+
+
+function AddIssue() {
+    const [addIssue, setAddIssue] = useState(true)
+
+    return (
+        <div className="add-issue">
+            <form>
+                <label>
+                    Description
+                <input type="text" placeholder="Description of Issue..." />
+                </label>
+
+                <label>
+                    Assign To
+                    <select name="forDev" id="forDev">
+                        <option value="Kenny B.">Kenny B.</option>
+                        <option value="Tyler B.">Tyler B.</option>
+                        <option value="Frank O.">Frank O.</option>
+                        <option value="Lonzo B.">Lonzo B.</option>
+                        <option value="Isaiah R.">Isaiah R.</option>
+                    </select>
+                </label>
+
+                <label>
+                    Priority
+                    <select name="priority" id="priority">
+                        <option value="Low">Low</option>
+                        <option value="Medium">Medium</option>
+                        <option value="High">High</option>
+                        <option value="Extra-High">Extra-High</option>
+                    </select>
+                </label>
+                <button type="submit">Add</button>
+
+            </form>
+        </div>
+    );
+}
+
+export default AddIssue;
