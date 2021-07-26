0a49c2e42f3378bf0022cae3580015f263122324
gagev <gagevank@gmail.com>
2021-07-25 23:29:16 -0400

    added test function

--- src/Components/CurrentIssue/CurrentIssue.js
+++ src/Components/CurrentIssue/CurrentIssue.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./CurrentIssue.css";
 
 
 
 function CurrentIssue() {
-    const [addIssue, setAddIssue] = useState(true)
+    
 
     return (
         <div className="current-issues">
